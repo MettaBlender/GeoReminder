56013c3c60ba4d7d7fa426193abd3750833b94f1
sleep404 <loris.stahlberg@icloud.com>
2025-07-02 16:41:11 +0200

    Update _layout.js

--- src/app/(tabs)/_layout.js
+++ src/app/(tabs)/_layout.js
@@ -48,6 +48,7 @@ export default function Layout() {
               name={name} 
               options={{
                 title,
+                headerTitle: () => <Header title={title} />,
                 tabBarIcon: ({ color, focused }) => (
                     <BottomnavIcon
                       name={focused ? iconFocused : icon} 
