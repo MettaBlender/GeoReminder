d830f7fcd499ee80f2843042485b9b9130da8f25
MettaBlender <153167243+MettaBlender@users.noreply.github.com>
2025-07-03 08:33:21 +0200

    Update _layout.js

--- src/app/(tabs)/_layout.js
+++ src/app/(tabs)/_layout.js
@@ -10,13 +10,15 @@ export default function Layout() {
     {
       name: "home/index",
       title: "Home",
+      titleTop: "Home",
       subtitle: "Alle deine Erinnerungen",
      icon: "home-outline",
     iconFocused: "home"
     },
     {
       name: "create/index",
-      title: "Erinnerung erstellen",
+      title: "Erstellen",
+      titleTop: "Erinnerung erstellen",
       subtitle: "Erstelle eine neue Erinnerung",
       icon: "add-outline",
     iconFocused: "add"
@@ -24,6 +26,7 @@ export default function Layout() {
     {
       name: "map/index",
       title: "Karte",
+      titleTop: "Karte",
       subtitle: "Alle Erinnerungen auf der Karte",
        icon: "map-outline",
     iconFocused: "map"
@@ -48,10 +51,11 @@ export default function Layout() {
           headerTitleAlign: 'center',
         }}
       >
-        {tabs?.map(({name, title, subtitle, icon, iconFocused}) => {
+        {tabs?.map(({name, titleTop, title, subtitle, icon, iconFocused}) => {
           return (
             <Tabs.Screen key={name} name={name} options={{
-              headerTitle: () => <Header title={title} subtitle={subtitle}/>,
+              title: title,
+              headerTitle: () => <Header title={titleTop} subtitle={subtitle}/>,
               tabBarIcon: ({ color, focused }) => (
                   <BottomnavIcon name={focused ? iconFocused : icon} color={color}/>
                 ),
